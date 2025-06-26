40c9dd1633cf1e64e943692c7a6d3eee4c09d23e
l4Imortal <148846250+l4Imortal@users.noreply.github.com>
2025-06-26 19:24:43 -0300

    Delete Prova-Ale-main directory

--- Prova-Ale-main/app/chatbot/chatbot_api.py
+++
@@ -1,5 +0,0 @@
-# Este é um mock simples para representar o chatbot.
-# Futuramente será substituído por integração real com Rasa/Dialogflow.
-
-def send_to_chatbot(message):
-    return f"Resposta do ChatBot para: {message}"
